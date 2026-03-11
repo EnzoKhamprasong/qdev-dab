0036ffadd3f25a2ea2a9a7ea6192436be375e529
Marek Felšöci <marek@felsoci.sk>
2026-03-11 21:52:45 +0100

    Implement a deposit method

--- dab/account.py
+++ dab/account.py
@@ -6,3 +6,7 @@ class Account:
 
   def __str__(self):
     return f"Account {self.number}:\n  - holder: {self.client}\n  - balance: {self.balance}\n"
+
+  def deposit(self, amount):
+    self.balance += amount
+    print("Deposit complete. New balance is %.2f" % self.balance)
