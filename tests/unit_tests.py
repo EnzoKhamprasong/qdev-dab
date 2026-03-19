c25bd0f9066942acf6bb011b18a141050a98d8fd
Enzo Khamprasong <22503774@me.uvsq.fr>
2026-03-19 11:31:32 +0100

    Ajout d'un test de retrait de nb négatif dans la fonction de test

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -6,7 +6,7 @@ class DABUnitTests(unittest.TestCase):
     self.assertEqual(checkWithdrawal(1000,1), True)
     self.assertEqual(checkWithdrawal(1000,600), False)
     self.assertEqual(checkWithdrawal(1000,5000000000), False)
-
+    self.assertEqual(checkWithdrawal(1000,-1), False)
     
 
 if __name__ == '__main__':
